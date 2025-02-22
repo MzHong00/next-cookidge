f9bcf9f6e1c9a5b62825cca282aca67588f96892
unknown <rkawk1539@gmail.com>
2025-02-22 21:01:16 +0900

    댓글 생성 UI 구현

---
+++ src/components/features/comment/create/createComment.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
+import type { IRecipe } from "@/types/recipe";
+import { IconBox } from "@/components/common/iconBox";
+
+import styles from "./createComment.module.scss";
+
+export const CreateComment = ({ recipeId }: { recipeId: IRecipe["_id"] }) => {
+  const [comment, setComment] = useState<string>("");
+
+  const onClickCreateComment = () => {
+    setComment("");
+  };
+
+  return (
+    <div className={styles.container}>
+      <textarea
+        value={comment}
+        placeholder="댓글을 입력하세요"
+        className={styles.textArea}
+        onChange={(e) => setComment(e.target.value)}
+        maxLength={100}
+      />
+      <button>
+        <IconBox className="main-button" onClick={onClickCreateComment}>
+          입력
+        </IconBox>
+      </button>
+    </div>
+  );
+};
