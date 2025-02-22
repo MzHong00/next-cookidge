5cba4915bfa756ca157560c8cd3d1d95bbf7b707
unknown <rkawk1539@gmail.com>
2025-02-22 21:08:09 +0900

    레시피 디테일 컴포넌트 구현

---
+++ src/containers/recipe/recipeDetail/background.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+import { PIdToURL } from "@/utils/pidToUrl";
+
+import styles from "./background.module.scss";
+
+const ANIMATE_DURATION = 2;
+
+interface Props {
+  id: string;
+  pidSrc?: string;
+}
+
+export function Background({ id, pidSrc }: Props) {
+  return (
+    <motion.div
+      layoutId={`thumbnail${id}`}
+      className={styles.background}
+      animate={{
+        filter: "blur(50px)",
+        scaleX: 1.2,
+        transition: { duration: ANIMATE_DURATION },
+      }}
+    >
+      <Image
+        src={PIdToURL(pidSrc || "")}
+        alt="background"
+        width={300}
+        height={300}
+        quality={50}
+      />
+    </motion.div>
+  );
+}
--- src/containers/recipe/recipeDetail/recipeDetail.tsx
+++ src/containers/recipe/recipeDetail/recipeDetail.tsx
@@ -1,52 +1,164 @@
 "use client";
 
-import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 import { motion } from "framer-motion";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { RiGroupLine } from "@react-icons/all-files/ri/RiGroupLine";
+import { RiTimer2Line } from "@react-icons/all-files/ri/RiTimer2Line";
+import { RiCalendarLine } from "@react-icons/all-files/ri/RiCalendarLine";
 
-import type { IUser } from "@/types/user";
 import type { IRecipe } from "@/types/recipe";
-import { PIdToURL } from "@/utils/pidToUrl";
+import type { IIngredient } from "@/types/ingredient";
+import { fadeSlide } from "@/lib/framer-motion";
+import { CurrentDateGap } from "@/utils/currentDateGap";
+import { INGREDIENT_CATEGORIES } from "@/constants/ingredient";
+import { RecipeQueries } from "@/services/recipe/queries/recipeQueries";
+import { IconBox } from "@/components/common/iconBox";
+import { Profile } from "@/components/common/profile";
 import { PictureSlider } from "@/components/common/pictureSlider";
+import { RecipeStep } from "@/components/features/recipe/step/recipeStep";
+import { LikeButton } from "@/components/features/recipe/like/likeButtton";
+import { CommentList } from "@/components/features/comment/read/commentList";
 
 import styles from "./recipeDetail.module.scss";
 
-interface Props {
-  recipe: IRecipe & { user: IUser };
-  backgroundWithPublicID?: string;
-}
+const SECTION = ["요리소개", "조리과정"] as const;
 
-export function RecipeDetail({ recipe, backgroundWithPublicID }: Props) {
-  const { _id, name, introduction, pictures, user } = recipe;
-  const {} = user;
-  
+export function RecipeDetail({ id }: { id: string }) {
+  const [tab, setTab] = useState<(typeof SECTION)[number]>(SECTION[0]);
+  const { data: recipe } = useSuspenseQuery(RecipeQueries.detailQuery(id));
+
+  const { ingredients, cooking_steps, user, ...contents } = recipe;
+  const { _id, name, picture } = user;
+
+  const changeTabHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setTab(e.currentTarget.innerText as (typeof SECTION)[number]);
+  };
+
   return (
-    <motion.div
-      transition={{
-        type: "spring",
-        stiffness: 80,
-        damping: 20,
-      }}
-    >
-      <motion.div className={styles.overview}>
-        <motion.div layoutId={`thumbnail${_id}`}>
-          <PictureSlider pictures={pictures} className={styles.pictureSlider} />
-        </motion.div>
-        <motion.div>
-          <h2>{name}</h2>
-          <p>{introduction}</p>
-        </motion.div>
-      </motion.div>
+    <div className="flex-column">
+      <motion.section
+        className={styles.card}
+        variants={fadeSlide}
+        initial="leftSlide"
+        animate="visible"
+      >
+        <Link href={`/user/${name}`}>
+          <Profile _id={_id} name={name} picture={picture} />
+        </Link>
 
-      <div>
-        <Image
-          src={PIdToURL(pictures[0] || backgroundWithPublicID || "")}
-          alt="background"
-          fill
-          quality={1}
-          priority
-          className={styles.background}
-        />
-      </div>
-    </motion.div>
+        <TabIndex tab={tab} onClick={changeTabHandler} />
+        
+        {tab === SECTION[0] && (
+          <motion.div
+            variants={fadeSlide}
+            initial="rightSlide"
+            animate="visible"
+          >
+            <Contents recipe={contents} />
+          </motion.div>
+        )}
+        {tab === SECTION[1] && (
+          <motion.div
+            variants={fadeSlide}
+            initial="leftSlide"
+            animate="visible"
+          >
+            <RecipeStep recipeSteps={cooking_steps} />
+          </motion.div>
+        )}
+      </motion.section>
+
+      <section>
+        <Ingredients ingredients={ingredients} />
+      </section>
+
+      <section>
+        <CommentList recipe_id={id} />
+      </section>
+    </div>
   );
 }
+
+const Contents = ({
+  recipe,
+}: {
+  recipe: Omit<IRecipe, "ingredients" | "cooking_steps">;
+}) => {
+  const {
+    _id,
+    name,
+    introduction,
+    pictures,
+    servings,
+    cooking_time,
+    like_members,
+    created_at,
+  } = recipe;
+
+  return (
+    <div className={styles.contentContainer}>
+      <PictureSlider pictures={pictures} className={styles.pictureSlider} />
+      <div className={styles.contents}>
+        <h2>{name}</h2>
+        <section>
+          <IconBox Icon={RiCalendarLine}>
+            {CurrentDateGap(created_at)}전
+          </IconBox>
+          <IconBox Icon={RiTimer2Line}>조리시간 {cooking_time}분</IconBox>
+          <IconBox Icon={RiGroupLine}>{servings}인분</IconBox>
+        </section>
+        <p>{introduction}</p>
+      </div>
+      <LikeButton recipe_id={_id} likeMembers={like_members} />
+    </div>
+  );
+};
+
+const Ingredients = ({
+  ingredients,
+}: {
+  ingredients: Pick<IIngredient, "category" | "name" | "quantity">[];
+}) => {
+  return (
+    <ul className={styles.ingredients}>
+      {ingredients.map(({ category, name, quantity }, i) => (
+        <motion.li
+          key={name}
+          variants={fadeSlide}
+          initial="leftSlide"
+          animate="visible"
+          transition={{ delay: i * 0.1 }}
+        >
+          <IconBox>
+            {INGREDIENT_CATEGORIES[category]} {name} {quantity}
+          </IconBox>
+        </motion.li>
+      ))}
+    </ul>
+  );
+};
+
+const TabIndex = ({
+  tab,
+  onClick,
+}: {
+  tab: (typeof SECTION)[number];
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}) => {
+  return (
+    <div className={styles.cardTabIndex}>
+      {SECTION.map((v) => (
+        <button key={v} onClick={onClick}>
+          <IconBox>
+            {v}
+            {v === tab && (
+              <motion.div layoutId="underline" className={styles.underline} />
+            )}
+          </IconBox>
+        </button>
+      ))}
+    </div>
+  );
+};
