827d33880ca084266d0e0c603b2e0bd633cd0dfe
unknown <rkawk1539@gmail.com>
2025-02-22 21:06:05 +0900

    재료 이모지 상수 배열 생성

---
+++ src/constants/ingredient.ts
@@ -0,0 +1,11 @@
+export const INGREDIENT_SORT_TYPES = ["카테고리", "유통기한"];
+export const INGREDIENT_TABLE_FIELD = ["분류", "이름", "수량", "유통기한"];
+export const INGREDIENT_CATEGORIES: Record<string, string> = {
+  고기: "🍖",
+  채소: "🥬",
+  과일: "🍎",
+  해산물: "🐠",
+  유제품: "🥛",
+  견과류: "🥜",
+  소스: "🧂",
+};
