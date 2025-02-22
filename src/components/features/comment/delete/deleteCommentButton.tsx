b73e9337e2231bf2a888c5c584bb9005c98700fd
unknown <rkawk1539@gmail.com>
2025-02-22 21:01:31 +0900

    댓글 제거 버튼 UI 구현

---
+++ src/components/features/comment/delete/deleteCommentButton.tsx
@@ -0,0 +1,12 @@
+import type { IRecipe } from "@/types/recipe";
+import type { IComment } from "@/types/comment";
+
+interface Props {
+  recipe_id: IRecipe["_id"];
+  author_id: IRecipe['author_id'];
+  comment_id: IComment["_id"];
+}
+
+export const DeleteCommentButton = ({ comment_id, recipe_id, author_id }: Props) => {
+  return <button >삭제</button>;
+};
