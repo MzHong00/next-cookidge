25f271a0aca7bc5ea0d7d6cc7d7d03eaf15f6cf3
unknown <rkawk1539@gmail.com>
2025-02-22 21:10:29 +0900

    레시피리스트 next/dynamic으로 클라이언트에서 다운로드

--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -1,22 +1,22 @@
 "use client";
 
-import { Suspense } from "react";
 import dynamic from "next/dynamic";
 
 import { RecipeSearchOption } from "@/containers/recipe/recipeSearchOption/recipeSearchOption";
 
 const RecipeList = dynamic(
-  () => import("@/containers/recipe/recipeList/recipeList"),
-  { ssr: false,loading:() => <div>기달</div> }
+  () =>
+    import("@/containers/recipe/recipeList/recipeList").then(
+      (m) => m.RecipeList
+    ),
+  { ssr: false }
 );
 
 export default function RecipePage() {
   return (
     <>
       <RecipeSearchOption />
-      <Suspense fallback="기달">
-        <RecipeList />
-      </Suspense>
+      <RecipeList />
     </>
   );
 }
