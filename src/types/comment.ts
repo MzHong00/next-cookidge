b4f9ef0899a4bb3d741f9abacef08f20ea9cdf7e
unknown <rkawk1539@gmail.com>
2025-02-22 21:06:36 +0900

    댓글 타입 생성

---
+++ src/types/comment.ts
@@ -0,0 +1,10 @@
+import type { IUser } from "./user";
+import type { IRecipe } from "./recipe";
+
+export interface IComment {
+  _id?: string;
+  recipe_id: IRecipe["_id"];
+  user_id: IUser["_id"];
+  comment: string;
+  created_at: Date;
+}
