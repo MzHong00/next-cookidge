cd6113e10d2d33d77a7a9e3d93ee263f903cb625
unknown <rkawk1539@gmail.com>
2025-04-17 21:15:59 +0900

    fix: 랭크 병렬 라우팅 제거 후 page.tsx로 처리

--- src/app/rank/layout.tsx
+++ src/app/rank/layout.tsx
@@ -1,44 +1,20 @@
-import { RiTrophyLine } from "@react-icons/all-files/ri/RiTrophyLine";
-
-import { IconBox } from "@/components/common/iconBox";
-
+import { ClientRender } from "@/components/common/clientRender";
 import styles from "./layout.module.scss";
 
 export default function RankLayout({
   search,
-  follow,
-  maker,
+  children,
 }: {
   search: React.ReactNode;
-  follow: React.ReactNode;
-  maker: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
-    <div className={styles.container}>
+    <div>
       <h2>사용자 탐색</h2>
 
-      <div className={styles.contents}>
-        <section>{search}</section>
-
-        <section>
-          <div className={styles.rankTitle}>
-            <h3>
-              <IconBox Icon={RiTrophyLine}>인기킹</IconBox>
-            </h3>
-            <p>팔로워가 가장 많은 사용자</p>
-          </div>
-          {follow}
-        </section>
-
-        <section>
-          <div className={styles.rankTitle}>
-            <h3>
-              <IconBox Icon={RiTrophyLine}>적극 참여킹</IconBox>
-            </h3>
-            <p>레시피를 가장 많이 작성한 사용자</p>
-          </div>
-          {maker}
-        </section>
+      <div className={styles.container}>
+        {search}
+        <ClientRender>{children}</ClientRender>
       </div>
     </div>
   );
