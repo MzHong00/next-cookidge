fb86f4a8dcd054b78c8383c251085e2110d36014
unknown <rkawk1539@gmail.com>
2025-05-06 17:08:05 +0900

    fix: pre-rendering build 에러 해결

--- src/app/me/update/page.tsx
+++ src/app/me/update/page.tsx
@@ -1,6 +1,7 @@
 import QueryHydrate from "@/components/common/queryHydrate";
 import { UserQueries } from "@/services/user/queries/userQueries";
 import { UserUpdate } from "@/containers/user/userUpdate/userUpdate";
+import { ClientRender } from "@/components/common/clientRender";
 
 export const metadata = {
   title: "프로필 편집",
@@ -11,7 +12,9 @@ export default async function MeUpdatePage() {
     <div>
       <h3 style={{ marginBottom: "2rem" }}>프로필 편집</h3>
       <QueryHydrate queryOptions={[UserQueries.meQuery()]}>
-        <UserUpdate />
+        <ClientRender>
+          <UserUpdate />
+        </ClientRender>
       </QueryHydrate>
     </div>
   );
--- src/containers/user/userUpdate/userUpdate.tsx
+++ src/containers/user/userUpdate/userUpdate.tsx
@@ -8,5 +8,5 @@ import { UserUpdateForm } from "@/components/features/user/update/userUpdateForm
 export const UserUpdate = () => {
   const { data: me } = useSuspenseQuery(UserQueries.meQuery());
 
-  return <>{me && <UserUpdateForm user={me} />}</>;
+  return me ? <UserUpdateForm user={me} /> : null;
 };
