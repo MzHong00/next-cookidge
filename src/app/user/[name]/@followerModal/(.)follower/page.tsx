758fb7b3bf22eccb7a6d4949e77b340bbb8fa4af
unknown <rkawk1539@gmail.com>
2025-05-12 20:07:42 +0900

    fix: 모달 에러 수정 및 nav z-index 모달보다 낮게

--- src/app/user/[name]/@followerModal/(.)follower/page.tsx
+++ src/app/user/[name]/@followerModal/(.)follower/page.tsx
@@ -1,6 +1,5 @@
 import UserFollowerPage from "../../follower/page";
 import { Dialog } from "@/components/common/dialog/dialog";
-import { ClientRender } from "@/components/common/clientRender";
 
 export default async function ModalFollower({
   params,
@@ -8,10 +7,8 @@ export default async function ModalFollower({
   params: Promise<{ name: string }>;
 }) {
   return (
-    <ClientRender>
-      <Dialog title="팔로워">
-        <UserFollowerPage params={params} />
-      </Dialog>
-    </ClientRender>
+    <Dialog title="팔로워">
+      <UserFollowerPage params={params} />
+    </Dialog>
   );
 }
