64689dab1fe474700d27e1a673f7618fa37cd80c
unknown <rkawk1539@gmail.com>
2025-05-15 20:07:00 +0900

    feat: 회원 탈퇴 기능 구현 및 프로필 편집 코드 일부 수정
    - 회원 탈퇴 기능 mutation 생성 및 컴포넌트 생성
    - 프로필 편집 컴포넌트 useSuspense에서 useQuery로 변경

--- src/app/me/update/page.tsx
+++ src/app/me/update/page.tsx
@@ -1,26 +1,18 @@
-import { getCookiesAsString } from "@/utils/getStringCookies";
-import QueryHydrate from "@/components/common/queryHydrate";
 import { UserUpdate } from "@/containers/user/userUpdate/userUpdate";
-import { ClientRender } from "@/components/common/clientRender";
-import { UserQueries } from "@/services/user/queries/userQueries";
+import { UserDeleteButton } from "@/components/features/user/delete/userDeleteButton";
+
+import styles from "./page.module.scss";
 
 export const metadata = {
   title: "프로필 편집",
 };
 
 export default async function MeUpdatePage() {
-  const cookies = await getCookiesAsString();
-
   return (
     <div>
-      <h3 style={{ marginBottom: "2rem" }}>프로필 편집</h3>
-      <QueryHydrate
-        queryOptions={[UserQueries.meQuery({ headers: { Cookie: cookies } })]}
-      >
-        <ClientRender>
-          <UserUpdate />
-        </ClientRender>
-      </QueryHydrate>
+      <h3 className={styles.pageTitle}>프로필 편집</h3>
+      <UserUpdate />
+      <UserDeleteButton className={styles.deleteButton} />
     </div>
   );
 }
